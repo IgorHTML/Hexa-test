4d3b370d44747fdb79874c0ac1d0c06e920e65c0
IgorHTML <klimklim136@gmail.com>
2018-09-08 11:14:03 +0300

    test

---
+++ app/js/common.js
@@ -0,0 +1,9 @@
+	// Carousel
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    items: 1,
+    nav: true,
+    smartSpeed: 700,
+    dots: true,
+    navText: [' <img src="../img/prev.png" alt="alt">','<img src="../img/next.png" alt="alt">'],
+  });
